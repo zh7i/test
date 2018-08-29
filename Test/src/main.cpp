fe56b6a83c730a292ece999b375e8fe84b3efab8
qi <zhuang7i@gmail.com>
2018-08-29 14:56:56 -0700

    First test

---
+++ Test/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+template <typename T1>struct TestFunctor1{
+	T1 operator()(T1 input) {
+		return input + input;
+	}
+};
+
+template <typename T1, typename T2>struct TestStruct1{
+	T1 operator()(T1 input, T2 functor) {
+		return functor(input);
+	}
+};
+
+int main(int argc, char **argv) {
+
+	TestFunctor1<int> functor;
+	TestStruct1<int, struct TestFunctor1<int>> test;
+
+	std::cout << test(1, functor) << std::endl;
+	return 0;
+}
