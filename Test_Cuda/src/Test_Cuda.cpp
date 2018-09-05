e8042ac6ba2d08e9c19a2741b4a4cf5f50b613a0
qi <zhuang7i@gmail.com>
2018-09-05 11:01:56 -0700

    first version cuda test

---
+++ Test_Cuda/src/Test_Cuda.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Test_Cuda.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <cuda_runtime.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+
+	cout << "" << endl; // prints 
+	return 0;
+}
