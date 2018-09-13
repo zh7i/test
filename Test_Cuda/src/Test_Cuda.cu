00cd49df8cb301c69960089458a43a3ca0a0bd7d
qi <zhuang7i@gmail.com>
2018-09-13 16:27:53 -0700

    Code refactor with templates

--- Test_Cuda/src/Test_Cuda.cu
+++ Test_Cuda/src/Test_Cuda.cu
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : Test_Cuda.cpp
+// Name        : Unordered Reduction.cpp
 // Author      : 
 // Version     :
 // Copyright   : 
@@ -11,14 +11,18 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
-
+#include <cmath>
+#include <limits>
+#include <algorithm>
 
 static void CheckCudaErrorAux (const char *, unsigned, const char *, cudaError_t);
 #define CUDA_CHECK_RETURN(value) CheckCudaErrorAux(__FILE__,__LINE__, #value, value)
 #define PER_THREAD_INIT_NUM 8
-#define PER_THREAD_SEG_NUM 16
-#define ARRAY_SIZE (1024 * 1024 * 128)
-#define MAX_NUM 256
+#define PER_THREAD_SEG_NUM 64
+#define ARRAY_SIZE (1024 * 64)
+#define MAX_NUM 4
+
+typedef float operand_type;
 
 template <typename T>
 struct SumOp {
@@ -30,11 +34,75 @@ struct SumOp {
 template <typename T>
 struct MaxOp {
     __device__ void operator()(T *addr, T value) {
-        atomicAdd(addr, value);
+        atomicMax(addr, value);
     }
 };
 
-__global__ void InitValue(int *input, int total_size, int value) {
+template<class T, typename std::enable_if<std::is_same<T, float>::value, void>::type* = nullptr>
+__device__ void MulOpByType(T *addr, T value)
+{
+    unsigned int *address = (unsigned int *)addr;
+    unsigned int value_old = *address, value_assumed;
+
+    do {
+        value_assumed = value_old;
+        value_old = atomicCAS(address,
+                              value_assumed,
+                              __float_as_uint(value * __uint_as_float(value_assumed)));
+    } while (value_assumed != value_old);
+}
+
+template<class T, typename std::enable_if<std::is_same<T, int>::value || std::is_same<T, unsigned int>::value, void>::type* = nullptr>
+__device__ void MulOpByType(T *addr, T value)
+{
+    unsigned int *address = (unsigned int *)addr;
+    unsigned int value_old = *address, value_assumed;
+
+    do {
+        value_assumed = value_old;
+        value_old = atomicCAS(address,
+                              value_assumed,
+                              (value * value_assumed));
+    } while (value_assumed != value_old);
+}
+
+template<class T, typename std::enable_if<std::is_same<T, double>::value, void>::type* = nullptr>
+__device__ void MulOpByType(T *addr, T value)
+{
+    unsigned long long int *address = (unsigned long long int *)addr;
+    unsigned long long int value_old = *address, value_assumed;
+
+    do {
+        value_assumed = value_old;
+        value_old = atomicCAS(address,
+                              value_assumed,
+                              __double_as_longlong(value * __longlong_as_double(value_assumed)));
+    } while (value_assumed != value_old);
+}
+
+template<class T, typename std::enable_if<std::is_same<T, long long int>::value || std::is_same<T, unsigned long long int>::value, void>::type* = nullptr>
+__device__ void MulOpByType(T *addr, T value)
+{
+    unsigned long long int *address = (unsigned long long int *)addr;
+    unsigned long long int value_old = *address, value_assumed;
+
+    do {
+        value_assumed = value_old;
+        value_old = atomicCAS(address,
+                              value_assumed,
+                              (value * value_assumed));
+    } while (value_assumed != value_old);
+}
+
+template <typename T>
+struct MulOp {
+    __device__ void operator()(T *addr, T value) {
+        MulOpByType(addr, value);
+    }
+};
+
+template <typename T>
+__global__ void InitValue(T *input, int total_size, T value) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int split_size = total_size / PER_THREAD_INIT_NUM;
 #pragma unroll
@@ -43,40 +111,44 @@ __global__ void InitValue(int *input, int total_size, int value) {
     }
 }
 
-template <typename ReductionF>
-__global__ void Segmentation(int *__restrict__ input_data, int *__restrict__ input_segment_id, int *__restrict__ output_segment_id, int segment_id_size, int total_size) {
+template <typename ReductionF, typename T>
+__global__ void Segmentation(T *__restrict__ input_data,
+                             int *__restrict__ input_segment_id,
+                             T *__restrict__ output_segment_result,
+                             int segment_result_size,
+                             int total_size) {
 
-    extern __shared__ int segment_result_slm[];
+    extern __shared__ T segment_result_slm[];
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int split_size = total_size / PER_THREAD_SEG_NUM;
 
-    if (threadIdx.x < segment_id_size) {
-        segment_result_slm[threadIdx.x] = 0;
+    if (threadIdx.x < segment_result_size) {
+        segment_result_slm[threadIdx.x] = 1;
     }
     __syncthreads();
 
-#pragma unroll
+//#pragma unroll
     for (int i = 0; i < PER_THREAD_SEG_NUM; i++) {
         int segment_id = input_segment_id[split_size * i + index];
-        int data_value = input_data[split_size * i + index];
-        atomicAdd(segment_result_slm + segment_id, data_value);
+        T data_value = input_data[split_size * i + index];
+        ReductionF()(segment_result_slm + segment_id, data_value);
     }
     __syncthreads();
 
-    if (threadIdx.x < segment_id_size) {
-        ReductionF()(output_segment_id + threadIdx.x, segment_result_slm[threadIdx.x]);
+    if (threadIdx.x < segment_result_size) {
+        ReductionF()(output_segment_result + threadIdx.x, segment_result_slm[threadIdx.x]);
     }
 }
 
 int main(int argc, char **argv) {
-    int *input_data = nullptr;
+    operand_type *input_data = nullptr;
     int *input_segment_id = nullptr;
-    int *output_segment_id = nullptr;
+    operand_type *output_segment_result = nullptr;
 
-    int *input_data_cpu = nullptr;
+    operand_type *input_data_cpu = nullptr;
     int *input_segment_id_cpu = nullptr;
-    int *output_segment_id_cpu = nullptr;
-    int *output_segment_id_gold = nullptr;
+    operand_type *output_segment_result_cpu = nullptr;
+    operand_type *output_segment_result_gold = nullptr;
     int array_num = ARRAY_SIZE;
     int segment_num = 512;
     int thread_num = 512;
@@ -87,14 +159,14 @@ int main(int argc, char **argv) {
     std::srand(std::time(nullptr));
     int dev = cudaSetDevice(0);
 
-    CUDA_CHECK_RETURN(cudaMalloc((void **)&input_data, array_num * sizeof(int)));
+    CUDA_CHECK_RETURN(cudaMalloc((void **)&input_data, array_num * sizeof(operand_type)));
     CUDA_CHECK_RETURN(cudaMalloc((void **)&input_segment_id, array_num * sizeof(int)));
-    CUDA_CHECK_RETURN(cudaMalloc((void **)&output_segment_id, segment_num * sizeof(int)));
+    CUDA_CHECK_RETURN(cudaMalloc((void **)&output_segment_result, segment_num * sizeof(operand_type)));
 
     CUDA_CHECK_RETURN(cudaEventCreate(&event1));
     CUDA_CHECK_RETURN(cudaEventCreate(&event2));
 
-    if ((input_data_cpu = new int[array_num]) == nullptr) {
+    if ((input_data_cpu = new operand_type[array_num]) == nullptr) {
         std::cout << "Allocation memory failure!" << std::endl;
         return -1;
     }
@@ -102,19 +174,20 @@ int main(int argc, char **argv) {
         std::cout << "Allocation memory failure!" << std::endl;
         return -1;
     }
-    if ((output_segment_id_cpu = new int[segment_num]) == nullptr) {
+    if ((output_segment_result_cpu = new operand_type[segment_num]) == nullptr) {
         std::cout << "Allocation memory failure!" << std::endl;
         return -1;
     }
-    if ((output_segment_id_gold = new int[segment_num]) == nullptr) {
+    if ((output_segment_result_gold = new operand_type[segment_num]) == nullptr) {
         std::cout << "Allocation memory failure!" << std::endl;
         return -1;
     }
     // Initialize array
     for (int i = 0; i < array_num; i++) {
-        input_data_cpu[i] = rand() % MAX_NUM;
+        input_data_cpu[i] = (float)rand() / RAND_MAX + 1.0f;
     }
-    CUDA_CHECK_RETURN(cudaMemcpy(input_data, input_data_cpu, array_num * sizeof(int), cudaMemcpyHostToDevice));
+
+    CUDA_CHECK_RETURN(cudaMemcpy(input_data, input_data_cpu, array_num * sizeof(operand_type), cudaMemcpyHostToDevice));
 
     for (int i = 0; i < array_num; i++) {
         input_segment_id_cpu[i] = rand() % segment_num;
@@ -122,10 +195,8 @@ int main(int argc, char **argv) {
     CUDA_CHECK_RETURN(cudaMemcpy(input_segment_id, input_segment_id_cpu, array_num * sizeof(int), cudaMemcpyHostToDevice));
 
 
-
-
     CUDA_CHECK_RETURN(cudaEventRecord(event1, 0));
-    InitValue<<<1, segment_num / PER_THREAD_INIT_NUM>>>(output_segment_id, segment_num, 0);
+    InitValue<operand_type><<<1, segment_num / PER_THREAD_INIT_NUM>>>(output_segment_result, segment_num, 1);
     CUDA_CHECK_RETURN(cudaEventRecord(event2, 0));
 
     CUDA_CHECK_RETURN(cudaEventSynchronize(event2));
@@ -135,7 +206,7 @@ int main(int argc, char **argv) {
 
 
     CUDA_CHECK_RETURN(cudaEventRecord(event1, 0));
-    Segmentation<SumOp<int>><<<array_num / PER_THREAD_SEG_NUM / (thread_num * 2), (thread_num * 2), segment_num * sizeof(int)>>>(input_data, input_segment_id, output_segment_id, segment_num, array_num);
+    Segmentation<MulOp<operand_type>, operand_type><<<array_num / PER_THREAD_SEG_NUM / (thread_num * 2), thread_num * 2, segment_num * sizeof(operand_type)>>>(input_data, input_segment_id, output_segment_result, segment_num, array_num);
     CUDA_CHECK_RETURN(cudaEventRecord(event2, 0));
 
     CUDA_CHECK_RETURN(cudaEventSynchronize(event2));
@@ -144,7 +215,7 @@ int main(int argc, char **argv) {
 
 
     CUDA_CHECK_RETURN(cudaEventRecord(event1, 0));
-    CUDA_CHECK_RETURN(cudaMemcpy(output_segment_id_cpu, output_segment_id, segment_num * sizeof(int), cudaMemcpyDeviceToHost));
+    CUDA_CHECK_RETURN(cudaMemcpy(output_segment_result_cpu, output_segment_result, segment_num * sizeof(operand_type), cudaMemcpyDeviceToHost));
     CUDA_CHECK_RETURN(cudaEventRecord(event2, 0));
 
     CUDA_CHECK_RETURN(cudaEventSynchronize(event2));
@@ -153,39 +224,56 @@ int main(int argc, char **argv) {
     std::cout << "Memory copy time: " << time << " ms" << std::endl;
 
     // compute and compare gold
-    memset(output_segment_id_gold, 0, segment_num * sizeof(int));
+    for (int i = 0; i < segment_num; i++) {
+        output_segment_result_gold[i] = 1;
+    }
 
     clock_t begin_clock = std::clock();
+    // sum reduction
+    /*
     for (int i = 0; i < array_num; i++) {
-        output_segment_id_gold[input_segment_id_cpu[i]] += input_data_cpu[i];
+        output_segment_result_gold[input_segment_id_cpu[i]] += input_data_cpu[i];
     }
+    */
+    // max reduction
+    /*
+    for (int i = 0; i < array_num; i++) {
+        output_segment_result_gold[input_segment_id_cpu[i]] = output_segment_result_gold[input_segment_id_cpu[i]] > input_data_cpu[i] ? output_segment_result_gold[input_segment_id_cpu[i]] : input_data_cpu[i];
+    }
+    */
+    // mul reduction
+    for (int i = 0; i < array_num; i++) {
+        output_segment_result_gold[input_segment_id_cpu[i]] *= input_data_cpu[i];
+    }
+
     clock_t end_clock = std::clock();
     double elapsed_cpu = double(end_clock - begin_clock) / CLOCKS_PER_SEC * 1000.0;
     std::cout << "CPU reduction time " << elapsed_cpu << " ms" << std::endl;
 
     for (int i = 0; i < segment_num; i++) {
-        if (output_segment_id_cpu[i] != output_segment_id_gold[i]) {
+        std::cout << "gpu result: " << output_segment_result_cpu[i] << " gold result: " << output_segment_result_gold[i] << std::endl;
+        if ((output_segment_result_cpu[i] != output_segment_result_gold[i])) {
             std::cout << "Error comparing with Gold! " << "Position " << i << std::endl;
             return -1;
         }
     }
-
     std::cout << "Test passed! " << std::endl;
 
+    // clean up
     if (input_data_cpu != nullptr) {
         delete[] input_data_cpu;
     }
     if (input_segment_id_cpu != nullptr) {
         delete[] input_segment_id_cpu;
     }
-    if (output_segment_id_cpu != nullptr) {
-        delete[] output_segment_id_cpu;
+    if (output_segment_result_cpu != nullptr) {
+        delete[] output_segment_result_cpu;
     }
-    if (output_segment_id_gold != nullptr) {
-        delete[] output_segment_id_gold;
+    if (output_segment_result_gold != nullptr) {
+        delete[] output_segment_result_gold;
     }
 
-    cudaFree(output_segment_id);
+    cudaFree(output_segment_result);
     cudaFree(input_segment_id);
     cudaFree(input_data);
     return 0;
